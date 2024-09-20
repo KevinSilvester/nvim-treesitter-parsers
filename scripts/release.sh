d93cb037f3c8a674d3058b3ab067f22e7a59db3b
Kevin Silvester <kevinsilvester05@gmail.com>
2024-09-20 02:25:29 +0100

    ci: enable creating release

---
+++ scripts/release.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+# A script to compile parsers for all platforms and package them in tar.gz archive.
+#
+# Order of operations
+# 1. Read the CHANGELOG:
+#  - Get the previous and current release tags
+#  - Get the list of the added/updated/deleted parsers
+#
+# 2. Check if there are release assets available for download for those previous release tag
+#
+# 3. Assets exist:
+#  - true:  Download the assets, extract archive, compile the updated/added parsers, delete the removed parsers, archive the parsers for upload
+#  - false: Compile all the parsers, archive them for upload
+
+set -x
+set -e
+
+ZIG_COMPILE_TARGETS=(
+   "x86_64-linux"
+   "aarch64-linux"
+   "x86_64-macos"
+   "aarch64-macos"
+   "x86_64-windows"
+)
+
+REPO_OWNER="KevinSilvester"
+REPO_NAME="nvim-treesitter-parsers"
+
+function _main() {
+   local current_release_tag=$(jq -r '.[0].tag' CHANGELOG.json)
+   local previous_release_tag=$(jq -r '.[1].tag' CHANGELOG.json)
+
+   local added_parsers=($(jq -r '.[0].changes.added[]' CHANGELOG.json))
+   local updated_parsers=($(jq -r '.[0].changes.updated[]' CHANGELOG.json))
+   local removed_parsers=($(jq -r '.[0].changes.removed[]' CHANGELOG.json))
+
+   for target in ${ZIG_COMPILE_TARGETS[@]}; do
+      local release_url=$(_release_url $previous_release_tag $target)
+
+      if _check_release_exists $release_url; then
+         _download_and_extract $release_url
+         _compile_parsers $target "added" ${added_parsers[@]}
+         _compile_parsers $target "updated" ${added_parsers[@]}
+         _delete_parsers ${removed_parsers[@]}
+         _archive_parsers $current_release_tag $target
+      else
+         _compile_all_parsers $target
+         _archive_parsers $current_release_tag $target
+      fi
+   done
+}
+
+function _check_release_exists() {
+   local url=$1
+
+   if [ $(curl -s -o /dev/null -w "%{http_code}" $url) -eq 404 ]; then
+      return 1
+   else
+      return 0
+   fi
+}
+
+function _release_url() {
+   local tag=$1
+   local target=$2
+   echo "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/$tag/parsers-$tag-$target.tar.gz"
+}
+
+function _download_and_extract() {
+   local url=$1
+
+   echo "::notice::Downloading and extracting $url"
+
+   mkdir -p /tmp
+   wget -qO- $url | tar xzf - -C /tmp
+}
+
+function _compile_parsers() {
+   local target=$1
+   local type=$2
+
+   echo "::notice::Compiling $type parsers: $@"
+   ts-parsers compile \
+      --no-fail-fast \
+      --compiler zig \
+      --target $target \
+      --destination /tmp/parser \
+      "$@"
+}
+
+function _delete_parsers() {
+   local removed_parsers=$1
+
+   if [[ -n $removed_parsers ]]; then
+      echo "::notice::Deleting removed parsers: ${removed_parsers[@]}"
+      for parser in "$@"; do
+         rm -r /tmp/parser/$parser
+      done
+   fi
+}
+
+function _compile_all_parsers() {
+   local target=$1
+
+   echo "::notice::Compiling all parsers"
+   ts-parsers compile \
+      --all \
+      --no-fail-fast \
+      --compiler zig \
+      --target $target \
+      --destination /tmp/parser
+}
+
+function _archive_parsers() {
+   local tag=$1
+   local target=$2
+
+   echo "::notice::Archiving parsers"
+   tar czf "parsers-$tag-$target.tar.gz" -C /tmp parser
+   rm -r /tmp/parser
+}
+
+_main
