5a4e258d2ee2670a2aa9813c4cf3f98446815a0c
Kevin Silvester <kevinsilvester05@gmail.com>
2025-02-01 16:25:58 +0000

    ci: update nvim stable release url

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,11 +5,11 @@ rm -rf nvim-linux64 nvim-treesitter
 git clone --depth=1 'https://github.com/nvim-treesitter/nvim-treesitter'
 # git --git-dir ./nvim-treesitter/.git rev-parse --short HEAD > ./tag
 
-wget 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz'
-tar xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+wget 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz'
+tar xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
-./nvim-linux64/bin/nvim --headless -u ./init.lua -c 'q'
+./nvim-linux-x86_64/bin/nvim --headless -u ./init.lua -c 'q'
 
 cat parsers.min.json | jq -M -S --indent 3 . > parsers.json
 
